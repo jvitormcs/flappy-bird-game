d9c41968160bc890c13b724a5093d124a033c086
jvitormcs <vitormouracs@gmail.com>
2021-09-02 18:54:41 -0300

    Inicio do Projeto

---
+++ jogo.js
@@ -0,0 +1,8 @@
+console.log('[JoãoVitor] Flappy Bird');
+
+const sprite = new Image();
+sprite.src = './sprite.png';
+
+const canvas = document.querySelector('canvas');
+const contexto = canvas.getContext('2d');
+
