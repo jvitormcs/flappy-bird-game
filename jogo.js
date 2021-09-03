7489e1e62ae3b5a30a96e214e5e574e5304c2fde
jvitormcs <vitormouracs@gmail.com>
2021-09-02 21:50:54 -0300

    Cenario Quase Pronto

--- jogo.js
+++ jogo.js
@@ -1,23 +1,95 @@
-console.log('[JoãoVitor] Flappy Bird');
+console.log('[JoaoVitor] Flappy Bird');
 
-const sprite = new Image();
-sprite.src = './sprite.png';
+const sprites = new Image();
+sprites.src = './sprites.png';
 
 const canvas = document.querySelector('canvas');
 const contexto = canvas.getContext('2d');
 
-function loop () {
-  
-  contexto.drawImage(
-    sprite,
-    0, 0, // Sprite X e Sprite Y
-    33, 24, // Tamanho de Recorte do Sprite
-    10, 50,
-    33, 24
-  );
+
+// [Plano de Fundo]
+const planoDeFundo = {
+  spriteX: 390,
+  spriteY: 0,
+  largura: 275,
+  altura: 204,
+  x: 0,
+  y: canvas.height - 204,
+  desenha() {
+    contexto.fillStyle = '#70c5ce';
+    contexto.fillRect(0,0, canvas.width, canvas.height)
+
+    contexto.drawImage(
+      sprites,
+      planoDeFundo.spriteX, planoDeFundo.spriteY,
+      planoDeFundo.largura, planoDeFundo.altura,
+      planoDeFundo.x, planoDeFundo.y,
+      planoDeFundo.largura, planoDeFundo.altura,
+    );
+
+    contexto.drawImage(
+      sprites,
+      planoDeFundo.spriteX, planoDeFundo.spriteY,
+      planoDeFundo.largura, planoDeFundo.altura,
+      (planoDeFundo.x + planoDeFundo.largura), planoDeFundo.y,
+      planoDeFundo.largura, planoDeFundo.altura,
+    );
+  },
+};
+
+// [Chao]
+const chao = {
+  spriteX: 0,
+  spriteY: 610,
+  largura: 224,
+  altura: 112,
+  x: 0,
+  y: canvas.height - 112,
+  desenha() {
+    contexto.drawImage(
+      sprites,
+      chao.spriteX, chao.spriteY,
+      chao.largura, chao.altura,
+      chao.x, chao.y,
+      chao.largura, chao.altura,
+    );
+
+    contexto.drawImage(
+      sprites,
+      chao.spriteX, chao.spriteY,
+      chao.largura, chao.altura,
+      (chao.x + chao.largura), chao.y,
+      chao.largura, chao.altura,
+    );
+  },
+};
+
+const flappyBird = {
+  spriteX: 0,
+  spriteY: 0,
+  largura: 33,
+  altura: 24,
+  x: 10,
+  y: 50,
+  desenha() {
+    contexto.drawImage(
+      sprites,
+      flappyBird.spriteX, flappyBird.spriteY, // Sprite X, Sprite Y
+      flappyBird.largura, flappyBird.altura, // Tamanho do recorte na sprite
+      flappyBird.x, flappyBird.y,
+      flappyBird.largura, flappyBird.altura,
+    );
+  }
+}
+
+function loop() {
+  planoDeFundo.desenha();
+  chao.desenha();
+  flappyBird.desenha();
+
+  flappyBird.y = flappyBird.y + 1;
 
   requestAnimationFrame(loop);
-
 }
 
 loop();
